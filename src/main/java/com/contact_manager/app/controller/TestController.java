723b3d979c532e06dbe2ac9844a433d7bd679828
Shahjahan <146447169+BhurgriShajahan@users.noreply.github.com>
2024-11-30 12:12:50 -0700

    Changin in Test controller

--- src/main/java/com/contact_manager/app/controller/TestController.java
+++ src/main/java/com/contact_manager/app/controller/TestController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 @Controller
 @RequestMapping("/api/v1/test")
 public class TestController {
